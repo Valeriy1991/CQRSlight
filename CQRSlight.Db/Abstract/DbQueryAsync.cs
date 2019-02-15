f24d6e50540df95da5942d573824011ca30a04ab
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2019-02-15 14:51:04 +0300

    Add async versions of all components

---
+++ CQRSlight.Db/Abstract/DbQueryAsync.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using CQRSlight.Abstract;
+using DbConn.DbExecutor.Abstract;
+
+namespace CQRSlight.Db.Abstract
+{
+    public abstract class DbQueryAsync<TContext, TResult> : IQueryAsync<TContext, TResult>
+    {
+        protected IDbExecutor DbExecutor { get; }
+
+        protected DbQueryAsync(IDbExecutor dbExecutor)
+        {
+            if (dbExecutor == null)
+                throw new ArgumentNullException(nameof(dbExecutor));
+            DbExecutor = dbExecutor;
+        }
+
+        public abstract Task<TResult> GetAsync(TContext context);
+    }
+
+    public abstract class DbQueryAsync<TResult> : IQueryAsync<TResult>
+    {
+        protected IDbExecutor DbExecutor { get; }
+
+        protected DbQueryAsync(IDbExecutor dbExecutor)
+        {
+            if (dbExecutor == null)
+                throw new ArgumentNullException(nameof(dbExecutor));
+            DbExecutor = dbExecutor;
+        }
+
+        public abstract Task<TResult> GetAsync();
+    }
+}
---
+++ CQRSlight/Abstract/ICommandAsync.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Ether.Outcomes;
+
+namespace CQRSlight.Abstract
+{
+    public interface ICommandAsync<in TCommandContext, TResult>
+    {
+        Task<IOutcome<TResult>> ExecuteAsync(TCommandContext commandContext);
+    }
+
+    public interface ICommandAsync<in TCommandContext>
+    {
+        Task<IOutcome> ExecuteAsync(TCommandContext commandContext);
+    }
+}
